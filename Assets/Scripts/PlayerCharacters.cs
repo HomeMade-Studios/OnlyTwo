bd84eb19c9da96ed4b6e00f2418aa838c9217f31
Romboide82 <ugo98.juve@libero.it>
2015-07-18 23:34:55 +0200

    c

---
+++ Assets/Scripts/PlayerCharacters.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public static class PlayerCharacters {
+
+	static Character[] characters = new Character[]{
+		new Character ("knight", 10f, 1, 5, 100),
+		new Character ("knight", 10f, 1, 5, 100),
+		new Character ("knight", 10f, 1, 5, 100),
+		new Character ("knight", 10f, 1, 5, 100)
+	};
+
+}
+
+public struct Character {
+
+	string characterName;
+	float movementSpeed;
+	int baseDamage;
+	int baseHealth;
+	int skillCooldownPercentage;
+
+	public Character(string characterName, float movementSpeed, int baseDamage, int baseHealth, int skillCooldownPercentage) {
+		this.characterName = characterName;
+		this.movementSpeed = movementSpeed;
+		this.baseDamage = baseDamage;
+		this.baseHealth = baseHealth;
+		this.skillCooldownPercentage = skillCooldownPercentage;
+	}
+
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	
+
+	void Update () {
+		Move ();
+	}
+
+	void Move () {
+		transform.Translate(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0f);
+	}
+}
